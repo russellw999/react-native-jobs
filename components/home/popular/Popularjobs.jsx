07d9435ba826752f55fdb440e85e3e600badb1a5
russellw999 <russellw999@gmail.com>
2023-03-29 14:33:11 +1100

    min 1.10 PopularJobs loadsAPI data not dummy data

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -15,14 +15,22 @@ import useFetch from '../../../hook/useFetch';
 const Popularjobs = () => {
   const router = useRouter();
 
-  const { data, isLoading, error, refetch } = useFetch('search',
+  const { data, isLoading, error } = useFetch("search",
      {
        query: 'React developer',
-       num_pages: 1
+       num_pages: "1"
      }
   );
 
+  const [selectedJob, setSelectedJob] = useState();
+
+
   console.log(data);
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id)
+  }
   
   return (
     <View style={styles.container}>
@@ -40,10 +48,12 @@ const Popularjobs = () => {
               <Text>Something went wrong</Text>
             ) : (
               <FlatList 
-                data={[1,2,3,4,5,6,7,8,9]}
+                data={data}
                 renderItem={({item}) => (
                   <PopularJobCard 
                       item={item}
+                      selectedJob={selectedJob}
+                      handleCardPress={handleCardPress}
                   />
                 )}
                 keyExtractor={item => item?.job_id}
